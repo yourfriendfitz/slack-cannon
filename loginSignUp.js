b7dfe29e52f895ee3999c301ea41bab6c5016037
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-29 13:12:24 -0500

    added google signIn button

--- loginSignUp.js
+++ loginSignUp.js
@@ -1,90 +1,93 @@
 const log = console.log;
 
-const [
-  signUpNameInput,
-  signUpEmailInput,
-  signUpPasswordInput,
-  signUpConfirmPasswordInput
-] = [
-  document.getElementById("signUpName"),
-  document.getElementById("signUpEmail"),
-  document.getElementById("signUpPassword"),
-  document.getElementById("signUpConfirmPassword")
-];
 
-const signUpButton = document.getElementById("signUpButton");
 
-const showInvalidOrValidInput = (input, bool) => {
-  if (bool) {
-    input.classList.remove("invalidInputSignUp");
-    input.classList.add("validInputSignUp");
-  } else {
-    input.classList.remove("validInputSignUp");
-    input.classList.add("invalidInputSignUp");
-  }
-};
+// const [
+//   signUpNameInput,
+//   signUpEmailInput,
+//   signUpPasswordInput,
+//   signUpConfirmPasswordInput
+// ] = [
+//   document.getElementById("signUpName"),
+//   document.getElementById("signUpEmail"),
+//   document.getElementById("signUpPassword"),
+//   document.getElementById("signUpConfirmPassword")
+// ];
 
-signUpNameInput.addEventListener("input", () => {
-  var input = signUpNameInput;
-  var is_not_blank = input.value;
-  showInvalidOrValidInput(input, is_not_blank);
-});
+// const signUpButton = document.getElementById("signUpButton");
 
-signUpEmailInput.addEventListener("input", () => {
-  const input = signUpEmailInput;
-  const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  const is_email = regex.test(input.value);
-  showInvalidOrValidInput(input, is_email);
-});
+// const showInvalidOrValidInput = (input, bool) => {
+//   if (bool) {
+//     input.classList.remove("invalidInputSignUp");
+//     input.classList.add("validInputSignUp");
+//   } else {
+//     input.classList.remove("validInputSignUp");
+//     input.classList.add("invalidInputSignUp");
+//   }
+// };
 
-signUpPasswordInput.addEventListener("input", () => {
-  const input = signUpPasswordInput;
-  const regex = /(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$/;
-  const is_password = regex.test(input.value);
-  showInvalidOrValidInput(input, is_password);
-});
+// signUpNameInput.addEventListener("input", () => {
+//   var input = signUpNameInput;
+//   var is_not_blank = input.value;
+//   showInvalidOrValidInput(input, is_not_blank);
+// });
 
-signUpConfirmPasswordInput.addEventListener("input", () => {
-  input = signUpConfirmPasswordInput;
-  if (signUpConfirmPasswordInput.value != signUpPasswordInput.value) {
-    showInvalidOrValidInput(input, false);
-  } else {
-    showInvalidOrValidInput(input, true);
-  }
-});
+// signUpEmailInput.addEventListener("input", () => {
+//   const input = signUpEmailInput;
+//   const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+//   const is_email = regex.test(input.value);
+//   showInvalidOrValidInput(input, is_email);
+// });
 
-const addUserDisplayName = async name => {
-  const user = await firebase.auth().currentUser;
-  await user
-    .updateProfile({
-      displayName: name
-    })
-    .then(function() {
-      // Update successful.
-    })
-    .catch(function(error) {
-      // An error happened.
-    });
-};
+// signUpPasswordInput.addEventListener("input", () => {
+//   const input = signUpPasswordInput;
+//   const regex = /(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$/;
+//   const is_password = regex.test(input.value);
+//   showInvalidOrValidInput(input, is_password);
+// });
 
-const createUserEmailPassword = async (name, email, password) => {
-  await firebase
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ...
-    });
-  await addUserDisplayName(name);
-};
+// signUpConfirmPasswordInput.addEventListener("input", () => {
+//   input = signUpConfirmPasswordInput;
+//   if (signUpConfirmPasswordInput.value != signUpPasswordInput.value) {
+//     showInvalidOrValidInput(input, false);
+//   } else {
+//     showInvalidOrValidInput(input, true);
+//   }
+// });
 
-signUpButton.addEventListener("click", async () => {
-  const [name, email, password] = [
-    signUpNameInput.value,
-    signUpEmailInput.value,
-    signUpPasswordInput.value
-  ];
-  await createUserEmailPassword(name, email, password);
-});
+// const addUserDisplayName = async name => {
+//   const user = await firebase.auth().currentUser;
+//   await user
+//     .updateProfile({
+//       displayName: name
+//     })
+//     .then(function() {
+//       // Update successful.
+//     })
+//     .catch(function(error) {
+//       // An error happened.
+//     });
+// };
+
+// const createUserEmailPassword = async (name, email, password) => {
+//   await firebase
+//     .auth()
+//     .createUserWithEmailAndPassword(email, password)
+//     .catch(function(error) {
+//       // Handle Errors here.
+//       var errorCode = error.code;
+//       var errorMessage = error.message;
+//       log(errorMessage, errorCode)
+//       // ...
+//     });
+//   await addUserDisplayName(name);
+// };
+
+// signUpButton.addEventListener("click", async () => {
+//   const [name, email, password] = [
+//     signUpNameInput.value,
+//     signUpEmailInput.value,
+//     signUpPasswordInput.value
+//   ];
+//   await createUserEmailPassword(name, email, password);
+// });
