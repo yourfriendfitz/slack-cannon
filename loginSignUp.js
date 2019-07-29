0e377bc726228a5b57499e8dade6c45ddae5479a
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-29 11:15:42 -0500

    added validation for signup inputs

--- loginSignUp.js
+++ loginSignUp.js
@@ -1,1 +1,52 @@
-const log = console.log
+const log = console.log;
+
+const [
+  signUpNameInput,
+  signUpEmailInput,
+  signUpPasswordInput,
+  signUpConfirmPasswordInput
+] = [
+  document.getElementById("signUpName"),
+  document.getElementById("signUpEmail"),
+  document.getElementById("signUpPassword"),
+  document.getElementById("signUpConfirmPassword")
+];
+
+const showInvalidOrValidInput = (input, bool) => {
+  if (bool) {
+    input.classList.remove("invalidInputSignUp");
+    input.classList.add("validInputSignUp");
+  } else {
+    input.classList.remove("validInputSignUp");
+    input.classList.add("invalidInputSignUp");
+  }
+};
+
+signUpNameInput.addEventListener("input", () => {
+  var input = signUpNameInput;
+  var is_not_blank = input.value;
+  showInvalidOrValidInput(input, is_not_blank);
+});
+
+signUpEmailInput.addEventListener("input", () => {
+  const input = signUpEmailInput;
+  const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const is_email = regex.test(input.value);
+  showInvalidOrValidInput(input, is_email);
+});
+
+signUpPasswordInput.addEventListener("input", () => {
+  const input = signUpPasswordInput;
+  const regex = /(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$/;
+  const is_password = regex.test(input.value);
+  showInvalidOrValidInput(input, is_password);
+});
+
+signUpConfirmPasswordInput.addEventListener("input", () => {
+  input = signUpConfirmPasswordInput;
+  if (signUpConfirmPasswordInput.value != signUpPasswordInput.value) {
+    showInvalidOrValidInput(input, false);
+  } else {
+    showInvalidOrValidInput(input, true);
+  }
+});
