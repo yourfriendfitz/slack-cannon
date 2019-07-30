7cb9b165b369a30823ae6cf1865f9c96ae114425
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-30 15:11:09 -0500

    fixed time for message display

--- global.js
+++ global.js
@@ -14,19 +14,52 @@ let userDetailsDiv = document.getElementById("userDetails");
 
 // displayProfileObject();
 
+function writeNewPost(message) {
+  // A post entry.
+  var postData = {
+    timestamp: Date.now(),
+    message: message,
+    userObj: JSON.stringify(firebase.auth().currentUser)
+  };
+
+  // Get a key for a new Post.
+  var newPostKey = firebase
+    .database()
+    .ref()
+    .child("global")
+    .push().key;
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  var updates = {};
+  updates["/global/" + newPostKey] = postData;
+
+  return firebase
+    .database()
+    .ref()
+    .update(updates);
+}
+
 var dataObj = {};
 var globalData = firebase.database().ref("global");
 globalData.on("value", function(snapshot) {
   userDetailsDiv.innerHTML = "";
   var dataObj = snapshot.val();
   var dataArray = Object.values(dataObj);
-  dataArray.forEach(obj => {
+  var keysArray = Object.keys(dataObj);
+  dataArray.forEach((obj, index) => {
     var userObj = JSON.parse(obj.userObj);
     // put finished HTML here for template literal
     var message = `<div class="userDetails">
       <img src="${userObj.photoURL}">
-      <p>${userObj.displayName}</p>
-      <button>Remove</button>
+      <p>${userObj.displayName} - ${new Date(obj.timestamp).toLocaleTimeString(
+      "en-US",
+      {
+        hour12: true,
+        hour: "numeric",
+        minute: "numeric"
+      }
+    )}</p>
+      <button onclick="deleteMessage('${keysArray[index]}')">Remove</button>
       <span>${obj.message}</span>
   </div> `;
     userDetailsDiv.insertAdjacentHTML("beforeend", message);
@@ -48,4 +81,11 @@ const addUserPhoto = async url => {
     .catch(function(error) {
       // An error happened.
     });
+};
+
+const deleteMessage = key => {
+  firebase
+    .database()
+    .ref(`global/${key}`)
+    .remove();
 };
