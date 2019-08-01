68af0c6c19afcd8a51f6b949d645527897f733fa
Full Name <nickfuentes24@gmail.com>
2019-08-01 15:39:56 -0500

    Added a sign out button and hamburger menu and toggle.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,24 @@
 const log = console.log;
 
+const cannonIcon = document.querySelector(".icon");
+
+const sidenav = document.querySelector(".sidenav");
+
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    
+    if (user.photoURL === null) {
+      addUserPhoto("https://i.imgur.com/JeMMr0v.png");
+    }
     return;
   } else {
     window.location.href = "login.html";
+  }
+});
+
+cannonIcon.addEventListener("click", () => {
+  if (sidenav.style.display === "flex") {
+    sidenav.style.display = "none";
+  } else {
+    sidenav.style.display === "flex";
   }
 });
