d0a2d811f3b405ade1e73f271db8919a57c49555
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-02 11:47:30 -0500

    added admin users

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 const log = console.log;
 
+let userIsAdmin = false;
+
 const cannonIcon = document.querySelector(".icon");
 
 const sidenav = document.querySelector(".sidenav");
@@ -8,6 +10,14 @@ firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     if (user.photoURL === null) {
       addUserPhoto("https://i.imgur.com/JeMMr0v.png");
+    }
+    if (
+      (user.email === "fitzgerald913@gmail.com") |
+      (user.email === "nickfuentes24@gmail.com") |
+      (user.email === "antonio.tobar.dev@gmail.com") |
+      (user.email === "jacob.bankston.smile@gmail.com")
+    ) {
+      userIsAdmin = true;
     }
     return;
   } else {
