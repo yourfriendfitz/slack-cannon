2e492e8c2ab0361da2f66368c0e5a7fe2577e098
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-02 14:07:12 -0500

    fixed events page

--- support.js
+++ support.js
@@ -2,11 +2,16 @@ const nameInput = document.querySelector("#fullName");
 
 const emailInput = document.querySelector("#email");
 
-document.addEventListener("DOMContentLoaded", () => {
-  nameInput.value = firebase.auth().currentUser
-    ? firebase.auth().currentUser.displayName
-    : "";
-  emailInput.value = firebase.auth().currentUser
-    ? firebase.auth().currentUser.email
-    : "";
+const autoFillInputs = () => {
+  nameInput.value = firebase.auth().currentUser.displayName || "";
+  emailInput.value = firebase.auth().currentUser.email || "";
+};
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    autoFillInputs();
+    return;
+  } else {
+    return;
+  }
 });
